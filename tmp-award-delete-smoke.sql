877cf225a6c2517a9587921347d6edd7aaaf2271
CAMAE <cameron@garrettandgarrett.info>
2026-03-04 21:29:17 -0600

    Chore: ignore and remove tmp/logs/backup artifacts from repo

--- tmp-award-delete-smoke.sql
+++
@@ -1,1 +0,0 @@
-﻿delete from public."CompanyBadge" where "companyId" = '746b2c20-4487-4da7-a76d-cc60cb546c9c';
--- tmp-award-verify-smoke.sql
+++
@@ -1,8 +0,0 @@
-﻿select count(*)::int as n
-from public."CompanyBadge"
-where "companyId" = '746b2c20-4487-4da7-a76d-cc60cb546c9c';
-
-select "id","badgeId","moduleId","awardedAt"
-from public."CompanyBadge"
-where "companyId" = '746b2c20-4487-4da7-a76d-cc60cb546c9c'
-order by "awardedAt" desc;
